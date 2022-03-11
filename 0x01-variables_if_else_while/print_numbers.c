05bc86c637e1161ca3de361b270dd8ba7cd7a6ec
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-11 05:02:40 -0800

    number

---
+++ 0x01-variables_if_else_while/print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = '0';
+	int i = 0;
+	/* your code goes there */
+	while (i < 10)
+	{
+		putchar(ch);
+		ch++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
