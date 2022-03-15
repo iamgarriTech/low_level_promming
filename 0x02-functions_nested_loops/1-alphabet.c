03b1750a98574b898623947adc516cf95c977b31
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-15 07:50:00 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+}
