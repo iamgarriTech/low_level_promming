a0ae526df7e3a45128750d4836eb2c6da92ef23f
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-16 04:28:59 -0700

    main

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,17 +4,17 @@
  * @n : times table to use
  * Return:void
  */
-void print_times_table(int n)	
+void print_times_table(int n)
 {
-	int a = 0, rep, b;
+int a = 0, rep, b;
 
-	if (n < 0 || n > 15)
-		return;
-	while (a <= n)
-		for (b = 0; b <= n; b++)
-		{
-			rep = a * b;
-			if (b == 0)
+if (n < 0 || n > 15)
+	return;
+while (a <= n)
+	for (b = 0; b <= n; b++)
+	{
+		rep = a * b;
+		if (b == 0)
 				_putchar('0' + rep);
 			else if (rep < 10)
 			{
@@ -25,7 +25,7 @@ void print_times_table(int n)
 			else if (rep < 100)
 			{
 				_putchar(' ');
-				_putchar('0' + rep / 10)
+				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
 			else
@@ -42,5 +42,4 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 		a++;
-	}
 }
