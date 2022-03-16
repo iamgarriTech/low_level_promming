43a5f8a07d48028fd42602fb91a39294042c6144
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-16 03:23:11 -0700

    main

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- *Description: Add two integers
- *Return result
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
-int add (int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
