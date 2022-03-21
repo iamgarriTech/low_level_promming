ab7d8aa3b066337ff80257ca875f0001afdf0f37
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-21 03:29:45 -0700

    corrected 3-puts.c the third time

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * Write a function that prints a string, followed by a new line, to stdout
- * @3-puts.c
- * Return: 0 always
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
 void _puts(char *str)
 {
 	int i = 0;
-	while(str[i])
+
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
