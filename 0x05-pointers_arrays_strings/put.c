4e7eccb8fe3dfef2856d9cc4fe8fb287d2f673b1
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-21 03:13:44 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/put.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Write a function that prints a string, followed by a new line, to stdout
+ * @3-puts.c
+ * Return: 0 always
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while(str[i])
+	{
+		puts(stdout)(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
