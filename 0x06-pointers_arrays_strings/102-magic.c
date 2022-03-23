8dfffd92785490a480c6865b54704bb83a85d285
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-23 14:22:28 -0700

    print_numbers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,19 +8,12 @@ int main(void)
 	a[2] = 98;
 	p = &n;
 	/*
-
 	 * write your line of code here...
-
 	 * Remember:
-
 	 * - you are not allowed to use a
-
 	 * - you are not allowed to modify p
-
 	 * - only one statement
-
 	 * - you are not allowed to code anything else than this line of code
-
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n **/
