7be0bd2514e119529dbfe2880aa7a00fd50e346a
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-23 12:50:32 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcat -  concatenates two strings.
  * @src: array with the string source
@@ -8,21 +7,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int c_src;
-	int c_dest;
-	int i_src;
-	int i_dest;
-
-	for (c_dest = 0; dest[c_dest] != 0; c_dest++)
-		for (c_src = 0; src[c_src] != 0; c_src++)
-			i_src = 0;
-	for (i_dest = c_dest; i_dest < (c_src + c_dest); i_dest++)
-	{
-		dest[i_dest] = src[i_src];
-		i_src++;
-	}
-	dest[c_src + c_dest] = 0;
+	int a = -1, i;
+	
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
 
 	return (dest);
-
 }
