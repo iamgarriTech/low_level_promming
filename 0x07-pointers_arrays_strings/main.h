3d6f3c612521d835770eb7a99573618e2e5b0f03
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-03-29 03:46:55 -0700

    finally

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,10 @@
 
 
 
+#include <stdio.h>
+
+
+
 int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
@@ -14,11 +18,15 @@ char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
-char *_strpbrk(char *s, char *accept);
-
 char *_strstr(char *haystack, char *needle);
 
+void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
+
 void print_chessboard(char (*a)[8]);
+
+char *_strpbrk(char *s, char *accept);
 
 
 
