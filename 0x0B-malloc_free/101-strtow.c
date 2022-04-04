d8c4e0d62e977424943a07554f9ee7c13735a639
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-04-04 04:51:25 -0700

    corrected 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,7 +28,7 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (c = height = 0; str[c] != '\0')
+	for (c = height = 0; str[c] != '\0'; c++)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
 	aout = malloc((height + 1) * sizeof(char *));
