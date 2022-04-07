4af962e653dcaf0c616a0ce5c1ffba00dc668b3e
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-04-07 05:08:18 -0700

    created 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of the space to malloc.
+ * Return: pointer with the address of the array created.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == '\0')
+	{
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+A readme file for 0x0C-more_malloc_free
